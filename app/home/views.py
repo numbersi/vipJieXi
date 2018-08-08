f5319b3d0d5394ff43eea7c02e875a00faf0e549
javaxx <136222866@qq.com>
2018-08-09 02:12:57 +0800

    更改 2pan 解析的host

--- app/home/views.py
+++ app/home/views.py
@@ -1,7 +1,8 @@
 #coding:utf8
+import re
 import requests
 from bs4 import BeautifulSoup
-from flask import request, jsonify
+from flask import request, jsonify ,render_template
 from . import home
 @home.route('/', methods=['GET', 'POST'])
 def hello_world():
@@ -10,40 +11,58 @@ def hello_world():
     url = apiUrl + url
     html =  getUrl_Baiyug(url)
 
-    if 'hdMd5' in html:
-        print('have hdMd5')
-        html = html.replace('src="baiyug', 'src="http://youkus.baiyug.cn:3697/vip_all/baiyug', -1)
-        html = html.replace('baiyug.php','baiyug')
-        print(html)
-        return html
-    cndJs  = { 'https://cdn.bootcss.com/dplayer/1.22.2/DPlayer.min.js' : 'js/DPlayer.min.js',
-   ' https://cdn.bootcss.com/dplayer/1.22.2/DPlayer.min.css': 'js/DPlayer.min.css',
-    'https://cdn.bootcss.com/hls.js/0.9.1/hls.min.js' :   'js/hls.min.js',
-    'https://cdn.bootcss.com/flv.js/1.4.2/flv.min.js': 'js/flv.min.js'
-               }
-    for key in cndJs:
-        html =  html.replace(cndJs[key],key)
-    print(html)
+
     return  html
 
 @home.route('/baiyug', methods=['POST'])
 def baiyug():
     datax = request.form.to_dict()
-
-    res = requests.post('http://youkus.baiyug.cn:3697/vip_all/baiyug.php',datax)
+    print(datax)
+    host = datax['host']
+    res = requests.post(host+'/vip_all/baiyug.php',datax)
     return res.content
 
-    print(datax)
-    return  'baiyug'
 def getUrl_Baiyug (url):
     if   url.startswith('/') :
-        url = 'http://yun.baiyug.cn'+ url
+        if url.startswith('//'):
+            url ='http:'+ url
+        else:
+            url = 'http://yun.baiyug.cn' + url
     obj = requests.get(url, allow_redirects=False)
     html = obj.content.decode('utf-8')
     htmlObj = BeautifulSoup(html, 'lxml')
     iframes = htmlObj.find('iframe')
     if iframes:
+        print('have')
         return getUrl_Baiyug( iframes['src'])
     else:
+        print('not have')
+        print(url)
+        host ='http://' + url.split('/')[2]
 
+        print(host)
+        if not html:
+            # print(url)
+            # res =  requests.get(url)
+            # HTML= res.content.decode('utf-8')
+            # print(url.split('/')[2])
+            # print(HTML)
+            # d = re.search(r'"(.+m3u8)', HTML).group(1)
+            # print(url.split('/')[2]+'/'+d)
+            return render_template('home/player.html')
+        if 'hdMd5' in html:
+            print('have hdMd5')
+            html = html.replace('src="baiyug', 'src="'+ host+ '/vip_all/baiyug', -1)
+            html = html.replace('href="baiyug', 'src="'+ host+ '/vip_all/baiyug', -1)
+            html = html.replace('baiyug.php', 'baiyug')
+            reStr = ' host :"{}" ,"md5"'.format(host)
+            html = re.sub(r'"md5"',reStr,html)
+            return html
+        cndJs = {'https://cdn.bootcss.com/dplayer/1.22.2/DPlayer.min.js': 'js/DPlayer.min.js',
+                 ' https://cdn.bootcss.com/dplayer/1.22.2/DPlayer.min.css': 'js/DPlayer.min.css',
+                 'https://cdn.bootcss.com/hls.js/0.9.1/hls.min.js': 'js/hls.min.js',
+                 'https://cdn.bootcss.com/flv.js/1.4.2/flv.min.js': 'js/flv.min.js'
+                 }
+        for key in cndJs:
+            html = html.replace(cndJs[key], key)
         return  html
